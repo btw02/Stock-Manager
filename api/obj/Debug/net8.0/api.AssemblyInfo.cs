5032ac9b8e3f01b401d92bef01ab8943eba3c50d
btw02 <bartosz.wilczak1@gmail.com>
2024-06-15 21:22:40 +0200

    Added User and Admin roles.

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df2fb1e1745dfc7d02f0f2212f19fa848c2d7451")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a4241cdc17920ee1e925fac9c1b33516f7d594e")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
